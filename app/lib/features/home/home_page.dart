2d9bcebc8068eb117b0552f78f77f8e7c2a23871
Jacob Hollings <hollingsjacob05@gmail.com>
2026-03-14 21:17:37 -0400

    Created listing card component and updated home page to display listing cards.

--- app/lib/features/home/home_page.dart
+++ app/lib/features/home/home_page.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import '../../core/providers/auth_provider.dart';
 import '../../core/widgets/app_bottom_nav.dart';
 import '../../core/widgets/app_header.dart';
 import '../../core/widgets/search_bar.dart';
+import '../../models/mock_data.dart';
+import '../../core/widgets/listing_card.dart';
 
 // HomePage is a ConsumerWidget so it can access Riverpod providers
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
-    @override
+  @override
   ConsumerState<HomePage> createState() => _HomePageState();
 }
 
@@ -24,7 +24,20 @@ class _HomePageState extends ConsumerState<HomePage> {
     super.dispose();
   }
 
-    @override
+  //filters listings based on search 
+  List<Listing> get _filteredListings {
+    //if search is emtpy just return all mock listings 
+    if (_searchQuery.isEmpty) return mockListings;
+    final String query = _searchQuery.toLowerCase();
+    // Filter listings by title course code or category
+    return mockListings.where((Listing listing) {
+      return listing.title.toLowerCase().contains(query) ||
+          listing.courseCode.toLowerCase().contains(query) ||
+          listing.category.toLowerCase().contains(query);
+    }).toList();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: const AppBottomNav(currentRoute: '/home'),
@@ -35,16 +48,16 @@ class _HomePageState extends ConsumerState<HomePage> {
             controller: _searchController,
             onChanged: (String value) => setState(() => _searchQuery = value),
           ),
+          //expanded widget to fill space with the listings
           Expanded(
-            child: Center(
-              child: FilledButton(
-                onPressed: () {
-                  ref.read(authProvider.notifier).state = false;
-                  context.go('/login');
-                },
-                child: const Text('Logout'),
-              ),
-            ),
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              itemCount: _filteredListings.length,
+              itemBuilder: (BuildContext context, int index) {
+                //display each listing usign the listingCard widget
+                return ListingCard(listing: _filteredListings[index]);
+              }
+            )
           ),
         ],
       ),
