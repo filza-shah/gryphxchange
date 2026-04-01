7a0b960a27a98211bd92bfcf4b335a4496b72466
Elliott Rahming <erahming@uoguelph.ca>
2026-04-01 08:13:06 -0400

    Edit create_listing_page.dart

--- app/lib/features/listing/create_listing_page.dart
+++ app/lib/features/listing/create_listing_page.dart
@@ -23,7 +23,7 @@ class CreateListingPage extends StatefulWidget {
 }
 
 class _CreateListingPageState extends State<CreateListingPage> {
-  static const String _googleBooksApiKey = 'AIzaSyBT9dVkVgkTjoNZFdXOYBw-bytus0P1BUg';
+  static const String _googleBooksApiKey = 'GOOGLE_BOOKS_API_KEY';
   static const int _maxImages = 1;
 
   // Accepted file types for listing photos.
